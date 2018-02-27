bbbb6bd07741d3dd900823cc4eb0ea44149ff725
Victor Serrano <victorserrano@MacBook-Pro-de-mac.local>
2018-02-27 10:00:36 +0100

    Nueva apiMusica 2

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -5,12 +5,25 @@
 
 /* LOCAL MAC MAMP*/
 
-return array(
+/*return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost:8889;dbname=apiMusica',
 			'username'   => 'root',
 			'password'   => 'root',
+		),
+	),
+);
+*/
+
+/*  PRODUCCION*/
+
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=localhost;dbname=victor',
+			'username'   => 'victor',
+			'password'   => 'LtNwkSmzk3zx5SJS',
 		),
 	),
 );
